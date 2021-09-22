0b6e45c13e1bc5592c130949f60628c2d8baf204
ydx1314 <1459546715@qq.com>
2021-09-22 11:01:08 +0800

    Site updated: 2021-09-22 11:01:07

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -2,7 +2,7 @@ var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
         var a = new Array
-        ("夜殿下", "茜茜公主", "雪糕", "love");
+        ("夜殿下", "茜茜公主", "雪糕", "love","冰冰","王咕咕");
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
